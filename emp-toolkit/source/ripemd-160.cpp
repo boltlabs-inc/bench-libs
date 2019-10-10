5bea30168a2e20af7296f8f703d0ecef0b65bc36
Marcella Hastings <marcellahastings@gmail.com>
2019-10-10 10:59:18 -0400

    add broken roll-shift function in emp-toolkit

--- emp-toolkit/source/ripemd-160.cpp
+++ emp-toolkit/source/ripemd-160.cpp
@@ -9,7 +9,22 @@ using namespace std;
 #define I(x,y,z) (((x)&(z)) | ((y) & ~(z)))
 #define J(x,y,z) ((x)^((y) | ~(z)))
 
-void test_basic_functions(int range = 1<<25, int runs = 100) {
+int rev(Integer x) {
+  return x.reveal<int>(PUBLIC);
+}
+
+/* ROL(x, n) cyclically rotates x over n bits to the left */
+/* x must be of an unsigned 32 bits type and 0 <= n < 32. */
+Integer inline ROL(Integer x, Integer n) {
+  Integer rollover = Integer(32, 32, PUBLIC) - n;
+  return (( x << n ) | ( x >> rollover )); 
+}
+
+Integer inline ROL(Integer x, int n) {
+  return (( x << n ) | ( x >> (32 - n))); 
+}
+
+void test_basic_functions(int range = 1<<25) {
   PRG prg(fix_key);
   long long x,y,z;
   prg.random_data(&x, 8);
@@ -28,9 +43,52 @@ void test_basic_functions(int range = 1<<25, int runs = 100) {
   assert ((H(x,y,z)) == (H(a,b,c).reveal<int>(PUBLIC)));
   assert ((I(x,y,z)) == (I(a,b,c).reveal<int>(PUBLIC)));
   assert ((J(x,y,z)) == (J(a,b,c).reveal<int>(PUBLIC)));
-
 }
 
+#define testROL(x,n)    (((x) << (n)) | ((x) >> (32-(n))))
+void test_rollover(int range = 1<<31) {
+  PRG prg(fix_key);
+  long long x,y,z,w;
+  prg.random_data(&x, 8);
+  prg.random_data(&y, 8);
+  prg.random_data(&z, 8);
+  x %= range;
+  y %= range;
+  z %= range;
+
+  Integer a(32, x, ALICE);
+  Integer b(32, y, BOB);
+  Integer c(32, z, BOB);
+
+  // test public/private versions are equal
+  for(int n=0; n<32; n++) {
+    Integer nI(32, n, PUBLIC);
+    assert (ROL(a, n).reveal<int>(PUBLIC) == ROL(a, nI).reveal<int>(PUBLIC));
+    assert (ROL(b, n).reveal<int>(PUBLIC) == ROL(b, nI).reveal<int>(PUBLIC));
+    assert (ROL(c, n).reveal<int>(PUBLIC) == ROL(c, nI).reveal<int>(PUBLIC));
+  }
+
+  // test behavior compared to reference impl
+  // TODO this fails at n=2
+  prg.random_data(&w, 8);
+  w %= range;
+  Integer d(32, w, ALICE);
+  for(int n=0; n<32; n++) {
+    assert( testROL(w,n) == ROL(d,n).reveal<int>(PUBLIC) );
+  }
+}
+
+// the ten basic operations
+#define FF(a, b, c, d, e, x, s) {\
+  (a) += F((b), (c), (d)) + (x);\
+  (a) = ROL((a), (s)) + (e);\
+  (c) = ROL((c), 10);\
+  }
+
+
+void test_basic_operations(int range = 1<<31, int runs=10) {
+  return;
+}
 
 template<typename Op, typename Op2>
 void test_int(int party, int range1 = 1<<25, int range2 = 1<<25, int runs = 100) {
@@ -70,8 +128,9 @@ int main(int argc, char** argv) {
 
 
 //	scratch_pad();return 0;
-    for (int i=0; i<100; i++) {
+    for (int i=0; i<1; i++) {
       test_basic_functions();
+      test_rollover();
     }
     /*
     test_int<std::plus<int>, std::plus<Integer>>(party);
