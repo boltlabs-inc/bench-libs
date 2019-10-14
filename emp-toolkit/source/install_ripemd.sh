ce4e16b6416f357a01b1b62b51d2e2957ba84d7a
Marcella Hastings <marcellahastings@gmail.com>
2019-10-14 18:32:09 -0400

    add test code for emp sha256

--- emp-toolkit/source/install_ripemd.sh
+++ emp-toolkit/source/install_ripemd.sh
@@ -1,9 +1,11 @@
-
+# apply patch to add complement operation for integers
 cd ~/emp-tool
 git apply ~/not_patch.diff
 make
 make install
 
+# build ripemd implementation
+# (currently broken, not slated for fixing b/c we don't need it)
 mv ~/ripemd-160.cpp ~/emp-sh2pc/test/
 
 cd ~/emp-sh2pc
@@ -12,5 +14,9 @@ mkdir build
 cd build
 cmake ..
 make ripemd-160
+
+# apply patch to update test code for ag2pc
+cd ~/emp-ag2pc
+git apply ~/test.patch
 
 
