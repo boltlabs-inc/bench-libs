1225af74a32c4563450428477e512a7647267c93
Marcella Hastings <marcellahastings@gmail.com>
2019-11-04 18:28:49 -0500

    clarify type issues in token api

--- emp-toolkit/source/tokens.h
+++ emp-toolkit/source/tokens.h
@@ -1,16 +1,28 @@
 /*
- * describes an API for calling EMP-toolkit (and other??) MPC functions
- *
+ * describes an API for calling MPC functions 
+ * 
  * to be integrated into Rust implementation
+ *
+ * This describes the basic high-level inputs we expect from the protocol.
+ * There is some additional precomputation that will happen in the clear,
+ * plus mangling of pretty Rust/C++ types to match the format used by
+ * the MPC frameworks
+ *
+ * TYPISSUE - There are some weird types here, as well. Everything has a type,
+ * but some of them are clearly incorrect 
+ * (e.g.a public key will not fit into a normal 32-bit integer). 
+ * but I don't know what representation they -will- take.
+ * I've marked such parameters with TYPISSUE
+ *
+ * Comments are sort of in doxygen style.
  */
 
-/* public and private key pair types */
-
-/* \param pubkey    : a public key. probably not an integer */
+/* ECDSA public and private key pair types */
+/* \param pubkey    : a public key. TYPISSUE - probably not an integer */
 struct PubKey{
   int pubkey;
 };
-/* \param privkey    : a private key. probably not an integer */
+/* \param privkey    : a private key. TYPISSUE - probably not an integer */
 struct PrivKey {
   int privkey;
 };
@@ -18,8 +30,8 @@ struct PrivKey {
 /* wallet type
  *
  * \param pkC           : customer public key 
- * \param wpk           : wallet public key (maybe not the same pubkey type?)
- * \param balance_cust  : customer balance
+ * \param wpk           : wallet public key (TYPISSUE - maybe not the same as as signing key?)
+ * \param balance_cust  : customer balance (TYPISSUE - do we want to allow larger transactions?)
  * \param balance_merch : merchant balance
  * \param txid_merch    : transaction ID for merchant close transaction (bits, formatted as they appear in the 'source' field of a transaction that spends it)
  * \param txid_escrow   : transaction ID for escrow transaction (ditto on format)
@@ -34,8 +46,8 @@ struct Wallet {
 };
 
 /* Commitment type
- * \param params    : parameters for the commitment scheme (not an int)
- * \param com       : commitment value
+ * \param params    : parameters for the commitment scheme (TYPISSUE - this will be a set of parameters including modulus q_com)
+ * \param com       : commitment value (TYPISSUE - this is an int mod q_com, may be bigger than 32 bits)
  */
 struct Commit {
   int params;
@@ -51,7 +63,7 @@ struct Commit {
  * option: port could be fixed in advance (not passed in here)
  * 
  * \param[in] pkM       : (shared) merchant public key
- * \param[in] amount    : (shared) transaction amount 
+ * \param[in] amount    : (shared) transaction amount (TYPISSUE)
  * \param[in] com_new   : (shared) commitment to new wallet object
  * \param[in] wpk_old   : (shared) previous wallet public key
  * \param[in] port      : (shared) communication port
@@ -59,13 +71,13 @@ struct Commit {
  *
  * \param[in] w_new     : (private) new wallet object
  * \param[in] w_old     : (private) previous wallet object
- * \param[in] t_new     : (private) commitment randomness
- * \param[in] pt_old    : (private) previous pay token (not an int)
+ * \param[in] t_new     : (private) commitment randomness (TYPISSUE)
+ * \param[in] pt_old    : (private) previous pay token (TYPISSUE - not an int)
  * \param[in] close_tx_escrow   : (private) bits of new close transaction (spends from escrow)
  * \param[in] close_tx_merch    : (private) bits of new close transaction (spends from merchant close transaction)
  * 
- * \param[out] ct_masked    : masked close token
- * \param[out] pt_masked    : masked pay token
+ * \param[out] ct_masked    : masked close token (TYPISSUE - definitely a pointer, maybe not an int)
+ * \param[out] pt_masked    : masked pay token (TYPISSUE - definitely a pointer, maybe not an int)
  *
  */
 void build_masked_tokens_cust(
@@ -73,6 +85,8 @@ void build_masked_tokens_cust(
   int amount,
   Commit com_new,
   PubKey wpk_old,
+  int port,
+  int ip_addr,
 
   Wallet w_new,
   Wallet w_old,
@@ -101,7 +115,7 @@ void build_masked_tokens_cust(
  * option: port could be fixed in advance (not passed in here)
  *
  * \param[in] pkM       : (shared) merchant public key
- * \param[in] amount    : (shared) transaction amount 
+ * \param[in] amount    : (shared) transaction amount (TYPISSUE)
  * \param[in] com_new   : (shared) commitment to new wallet object
  * \param[in] wpk_old   : (shared) previous wallet public key
  * \param[in] port      : (shared) communication port
@@ -117,6 +131,8 @@ void build_masked_tokens_merch(
   int amount,
   Commit com_new,
   PubKey wpk_old,
+  int port,
+  int ip_addr,
 
   PrivKey skM
 );
